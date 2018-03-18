a5479274ab17998f293a42e5143bb66a9d066592
tancou <tancou@gmx.fr>
2018-03-18 01:30:43 +0100

    Correctif javadoc.

--- MainLib/src/mainlib/controller/LNotifiable.java
+++ MainLib/src/mainlib/controller/LNotifiable.java
@@ -4,6 +4,11 @@ package mainlib.controller;
  * Controller notifiable interface.
  *
  * Allow Controllers to notify each other without initializing Threads.
+ *
+ * @author  Tancrède SUARD 11505293
+ * @author  Pierre-Elliott THIBOUD 11402690
+ * @version 1.0
+ * @since   2018-01-17
  */
 public interface LNotifiable {
     void whenNotified();
